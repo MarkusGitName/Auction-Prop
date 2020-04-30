eaf40c3c14bdf20f25ff0eb3456b8be20a9f4aeb
JP Herbst <jpherbst57@gmail.com>
2020-04-30 12:18:08 +0200

    Started working on the Auction Listing Page. This is where all properties going for Auction will be listed. Still working out my styling code at this point.

---
+++ AuctionProp_Buyers/Controllers/AuctionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using AuctionProp_Buyers.Models;
+using Newtonsoft.Json;
+
+namespace AuctionProp_Buyers.Controllers
+{
+    public class AuctionController : Controller
+    {
+        // GET: Auction
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
